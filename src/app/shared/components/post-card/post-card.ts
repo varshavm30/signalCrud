ca9a3b5612a69132cfd63167e5204a82f430b7c2
varshavm30 <varshamahajan9222@gmail.com>
2026-04-14 11:43:13 +0530

    worked on ui

--- src/app/shared/components/post-card/post-card.ts
+++ src/app/shared/components/post-card/post-card.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Ipost } from '../../models/posts';
 import { MATERIAL_IMPORTS } from '../../const/material';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-post-card',
-  imports: [...MATERIAL_IMPORTS],
+  imports: [...MATERIAL_IMPORTS, RouterLink],
   templateUrl: './post-card.html',
   styleUrl: './post-card.scss',
 })
